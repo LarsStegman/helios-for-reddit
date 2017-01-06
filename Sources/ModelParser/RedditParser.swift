795a1a5fcdc96c9be7f7e3d283d654c68cb7c7d5
Lars Stegman <l.s.stegman@student.tudelft.nl>
2017-01-06 09:34:49 +0100

    Create generic Reddit Parser

--- Sources/ModelParser/RedditParser.swift
+++ Sources/ModelParser/RedditParser.swift
@@ -10,6 +10,24 @@ import Foundation
 
 
 public class RedditParser {
+    class func parse(object json: [String: Any]) -> RedditTyped? {
+        guard let kindStr = json["kind"] as? String,
+            let kind = Kind(rawValue: kindStr.lowercased()),
+            let data = json["data"] as? [String: Any] else {
+                return nil
+        }
+
+        switch kind {
+        case .listing: return Listing(json: data)
+        case .comment: return Comment(json: data)
+        case .subreddit: return Subreddit(json: data)
+        case .link: return Link(json: data)
+        case .more: return More(json: data)
+        default: return nil
+        }
+
+    }
+
     class func parseThing(object json: [String: Any]) -> Thing? {
         guard let kindStr = json["kind"] as? String,
             let kind = Kind(rawValue: kindStr.lowercased()),
@@ -24,6 +42,5 @@ public class RedditParser {
         case .more: return More(json: data)
         default: return nil
         }
-
     }
 }
