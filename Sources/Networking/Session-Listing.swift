27b8b136fc86d6c4a1c7a943bbdffeebafd79125
Lars Stegman <l.s.stegman@student.tudelft.nl>
2017-02-05 00:37:39 +0100

    Add documentation for listing api

--- Sources/Networking/Session-Listing.swift
+++ Sources/Networking/Session-Listing.swift
@@ -9,6 +9,15 @@
 import Foundation
 
 extension Session {
+    
+    /// Loads the next data slice.
+    /// If there is no `after` in the listing, the completionHandler will be called with an 
+    /// `SessionError.noResult` error.
+    ///
+    /// - Parameters:
+    ///   - listing: The listing to load from
+    ///   - numberOfAlreadyLoadedComponents: The number of already read items
+    ///   - completionHandler: Called with the result or an error.
     public func next(listing: Listing, numberOfAlreadyLoadedComponents: Int? = nil,
                      completionHandler: @escaping ResultHandler<Listing>) {
         guard let after = listing.after,
@@ -26,6 +35,14 @@ extension Session {
         loadListing(from: source, query: queries, completionHandler: completionHandler)
     }
 
+    /// Loads the previous data slice.
+    /// If there is no `before` in the listing, the completionHandler will be called with an
+    /// `SessionError.noResult` error.
+    ///
+    /// - Parameters:
+    ///   - listing: The listing to load from
+    ///   - numberOfAlreadyLoadedComponents: The number of already read items
+    ///   - completionHandler: Called with the result or an error.
     public func previous(listing: Listing, numberOfAlreadyLoadedComponents: Int? = nil,
                          completionHandler: @escaping ResultHandler<Listing>) {
         guard let before = listing.before,
@@ -43,13 +60,16 @@ extension Session {
         loadListing(from: source, query: queries, completionHandler: completionHandler)
     }
 
+    /// Loads a listing from the provided url. If the url does not contain a listing, the 
+    /// completionHandler will be called with an `SessionError.invalidReponse` error.
+    ///
+    /// - Parameters:
+    ///   - url: The url to retrieve the listing from
+    ///   - query: Query parameters for the listing, e.g. `before`
+    ///   - completionHandler: Called with the result or an error.
     func loadListing(from url: URL, query: [URLQueryItem]? = nil,
                      completionHandler: @escaping ResultHandler<Listing>) {
-        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
-            completionHandler(nil, .invalidSource)
-            return
-        }
-
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
         if let queries = query {
             components.queryItems = queries
         }
